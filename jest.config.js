71e5584e94bb38a1670b0760a7ea539241afa201
andrewdyer <andrewdyer@outlook.com>
2024-07-26 14:33:52 +0100

    refactor: :hammer: remove deprecated globals configuration for ts-jest
    Refactored to include the ts-jest options directly within an array. This is now the preferred way to configure ts-jest

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,13 @@ module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     transform: {
-        '^.+\\.tsx?$': 'ts-jest'
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: 'tsconfig.json'
+            }
+        ]
     },
     testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-    globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.json'
-        }
-    },
     setupFilesAfterEnv: ['./jest.setup.ts']
 };
